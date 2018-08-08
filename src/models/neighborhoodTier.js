0806c6306d58cfd68e24e123815dadccc59cb85f
codyjett <codyjett@uab.edu>
2018-08-08 14:03:54 -0500

    Updated Tables w/ Colors

---
+++ src/models/neighborhoodTier.js
@@ -0,0 +1,81 @@
+export default [
+  {
+    neighborhood: 'Airport Highlands',
+    rank: 14,
+    tier: 'bottom'
+  },
+  {
+    neighborhood: 'Belview Heights',
+    rank: 5,
+    tier: 'top'
+  },{
+    neighborhood: 'East Lake',
+    rank: 15,
+    tier: 'bottom'
+  },{
+    neighborhood: 'East Thomas',
+    rank: 8,
+    tier: 'middle'
+  },{
+    neighborhood: 'Ensley',
+    rank: 13,
+    tier: 'middle'
+  },{
+    neighborhood: 'Hooper City',
+    rank: 20,
+    tier: 'bottom'
+  },{
+    neighborhood: 'Liberty Highlands',
+    rank: 9,
+    tier: 'middle'
+  },{
+    neighborhood: 'North Titusville',
+    rank: 12,
+    tier: 'middle'
+  },{
+    neighborhood: 'Overton',
+    rank: 3,
+    tier: 'top'
+  },{
+    neighborhood: 'Riley',
+    rank: 16,
+    tier: 'top'
+  },{
+    neighborhood: 'Rising West Princeton',
+    rank: 10,
+    tier: 'middle'
+  },{
+    neighborhood: 'Roebuck',
+    rank: 4,
+    tier: 'top'
+  },{
+    neighborhood: 'Roosevelt',
+    rank: 7,
+    tier: 'top'
+  },{
+    neighborhood: 'Smithfield Estates',
+    rank: 6,
+    tier: 'top'
+  },{
+    neighborhood: 'Sun Valley',
+    rank:2,
+    tier: 'top'
+  },{
+    neighborhood: 'Tuxedo',
+    rank: 11,
+    tier: 'middle'
+  },{
+    neighborhood: 'Wahouma',
+    rank: 17,
+    tier: 'bottom'
+  },{
+    neighborhood: 'West Goldwire',
+    rank: 1,
+    tier: 'top'
+  },{
+    neighborhood: 'Woodlawn',
+    rank: 18,
+    tier: 'bottom'
+  },
+
+]
--- src/routes/NeighborhoodPage.js
+++ src/routes/NeighborhoodPage.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import { connect } from 'dva';
+import { Layout, Table, Pagination } from 'antd';
+import styles from './styles.css';
 import AppLayout from '../components/AppLayout';
+import data from '../models/neighborhoodTier';
+
+
+const columns = [{
+  title: 'Neighborhood',
+  dataIndex: 'neighborhood',
+}, {
+  title: 'Rank',
+  dataIndex: 'rank',
+}, {
+  title: 'Tier',
+  dataIndex: 'tier',
+  render: (tier) => {
+    console.log(styles);
+    return (
+      <div className={styles[`cell-tier`] + ' ' + styles[`cell-tier-${tier.toLowerCase()}`]}>
+        {tier.toUpperCase()}
+      </div>
+    );
+  }
+}];
 
 function NeighborhoodPage() {
   return (
     <AppLayout>
-      NeighborhoodPage
+      <Layout.Header>
+      </Layout.Header>
+
+      <Table
+        columns={columns}
+        dataSource={data}
+        pagination={false} />
     </AppLayout>
-  );
+  )
 }
 
 NeighborhoodPage.propTypes = {
