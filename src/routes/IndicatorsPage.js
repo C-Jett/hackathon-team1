c5a4e38d51898f54e63913dea98a3de8442e5d21
Victor Nguyen <victor.nguyen@bbva.com>
2018-08-08 15:04:34 -0500

    Single page

--- src/routes/IndicatorsPage.js
+++ src/routes/IndicatorsPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { connect } from 'dva';
 import { Layout } from 'antd';
-import AppLayout from '../components/AppLayout';
 import { Table  } from 'antd';
+import AppLayout from '../components/AppLayout';
 import indicatorsData from '../models/indicators';
 
 const columns = [{
