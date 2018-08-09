a6da4b2115730d603311e55bb46f3539f1593be1
codyjett <codyjett@uab.edu>
2018-08-09 13:02:38 -0500

    Added functionality to map markers

--- src/components/MapPointer.js
+++ src/components/MapPointer.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { Icon } from 'antd';
 
-const IconText = ({ type, text }) => (
-  <span>
-    <Icon type={type} style={{ marginRight: 8 }} />
-    {text}
+const IconText = ({ path, type, text }) => (
+  <span onClick={() => window.location = `/#/neighborhood/${path}`}>
+    <Icon type={type} style={{color: 'red', fontSize: 25}} />
+    <span style={{
+        fontSize: 14,
+        alignItems:'center',
+        backgroundColor: '#fff',
+        padding: 2,
+        borderRadius: 5
+      }
+    }>
+      {text}
+    </span>
   </span>
+
+
 
 );
 function MapPointer({ neighborhood }) {
-
+  const path = neighborhood.toLowerCase().replace(' ', '-');
   return (
-    <IconText key={neighborhood} type="environment" text={neighborhood} />
+    <IconText key={neighborhood} type="environment" text={neighborhood}
+      path={path}
+    />
   );
 };
 
