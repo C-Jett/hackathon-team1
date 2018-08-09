bcb7b630dd6dca5a57e354812614fa2b0b758ca3
Victor Nguyen <victor.nguyen@bbva.com>
2018-08-09 11:19:25 -0500

    Radar chart (will still need a button to access it)

--- src/models/indicators.js
+++ src/models/indicators.js
@@ -1,202 +1,252 @@
 export default [{
     key: '1',
     indicator: "Concentrated Poverty",
-    description: "Proportion of residents at or below the national poverty level."
+    description: "Proportion of residents at or below the national poverty level.",
+    domain: "Demographic"
   }, {
     key: '2',
     indicator: "Income Inequality",
-    description: "A measure of the distribution of income (i.e., the gap between rich and poor)."
+    description: "A measure of the distribution of income (i.e., the gap between rich and poor).",
+    domain: "Demographic"
   }, {
     key: '3',
     indicator: "Life Expectancy",
-    description: "Life expectancy is the average number of years an individual can expect to live."
+    description: "Life expectancy is the average number of years an individual can expect to live.",
+    domain: "Demographic"
   }, {
     key: '4',
     indicator: "Park Quality",
-    description: "Proportion of residents at or below the national poverty level."
+    description: "Proportion of residents at or below the national poverty level.",
+    domain: "Demographic"
   }, {
     key: '5',
     indicator: "Population",
-    description: "Number of residents within the neighborhood compared to the overall city."
+    description: "Number of residents within the neighborhood compared to the overall city.",
+    domain: "Demographic"
   }, {
     key: '6',
     indicator: "Racial and Ethnic Diversity",
-    description: "Measure of the extent to which several different racial and ethnic groups are present in a community."
+    description: "Measure of the extent to which several different racial and ethnic domains are present in a community.",
+    domain: "Demographic"
   }, {
     key: '7',
     indicator: "Abandoned Structures",
-    description: "Proportion of properties that mail has not been picked up from or delivered to in more than 90 days."
+    description: "Proportion of properties that mail has not been picked up from or delivered to in more than 90 days.",
+    domain: "Blight"
   }, {
     key: '8',
     indicator: "Tax Delinquent Properties",
-    description: "The proportion of properties that are tax delinquent in a neighborhood."
+    description: "The proportion of properties that are tax delinquent in a neighborhood.",
+    domain: "Blight"
   }, {
     key: '9',
     indicator: "Visual Property Nuisances",
-    description: "The proportion of properties that are deteriorated, dilapidated, and overgrown in a neighborhood."
+    description: "The proportion of properties that are deteriorated, dilapidated, and overgrown in a neighborhood.",
+    domain: "Blight"
   }, {
     key: '10',
     indicator: "Access to Mainstream Financial Services",
-    description: "Proportion of un- and underbanked households in the neighborhood."
+    description: "Proportion of un- and underbanked households in the neighborhood.",
+    domain: "Economic"
   }, {
     key: '11',
     indicator: "Business Retention",
-    description: "Percent change in number of neighborhood businesses from the previous year."
+    description: "Percent change in number of neighborhood businesses from the previous year.",
+    domain: "Economic"
   }, {
     key: '12',
     indicator: "Local Business Vitality",
-    description: "Proportion of small (0-4 employees), locally-owned businesses within a neighborhood."
+    description: "Proportion of small (0-4 employees), locally-owned businesses within a neighborhood.",
+    domain: "Economic"
   }, {
     key: '13',
     indicator: "Payday Loans",
-    description: "The number of payday loans per 1,000 residents within one mile of a neighborhood."
+    description: "The number of payday loans per 1,000 residents within one mile of a neighborhood.",
+    domain: "Economic"
   }, {
     key: '14',
     indicator: "Adult Educational Attainment",
-    description: "Proportion of neighborhood adults, aged 25 and older, with a high school diploma (or equivalent)."
+    description: "Proportion of neighborhood adults, aged 25 and older, with a high school diploma (or equivalent).",
+    domain: "Educational"
   }, {
     key: '15',
     indicator: "High School Graduation Rate",
-    description: "Proportion of students entering neighborhood secondary schools that graduate."
+    description: "Proportion of students entering neighborhood secondary schools that graduate.",
+    domain: "Educational"
   }, {
     key: '16',
     indicator: "Preschool Enrollment",
-    description: "Proportion of three and four year-olds in the neighborhood enrolled in preschool."
+    description: "Proportion of three and four year-olds in the neighborhood enrolled in preschool.",
+    domain: "Educational"
   }, {
     key: '17',
     indicator: "Reading Proficiency",
-    description: "Proportion of third or fourth grade students meeting or exceeding \"proficient\" reading levels on standardized assessments in neighborhood schools."
+    description: "Proportion of third or fourth grade students meeting or exceeding \"proficient\" reading levels on standardized assessments in neighborhood schools.",
+    domain: "Educational"
   }, {
     key: '18',
     indicator: "School Readiness Scores",
-    description: "Proportion of neighborhood kindergarteners prepared for first grade."
+    description: "Proportion of neighborhood kindergarteners prepared for first grade.",
+    domain: "Educational"
   }, {
     key: '19',
     indicator: "Employment Rate",
-    description: "Proportion of neighborhood working age residents (15 - 64 years) employed."
+    description: "Proportion of neighborhood working age residents (15 - 64 years) employed.",
+    domain: "Employment"
   }, {
     key: '20',
     indicator: "Long-Term Unemployment",
-    description: "Proportion of the neighborhood’s working population (i.e., age 16 and up) unemployed for more than 12 months."
+    description: "Proportion of the neighborhood’s working population (i.e., age 16 and up) unemployed for more than 12 months.",
+    domain: "Employment"
   }, {
     key: '21',
     indicator: "Public Assisted Households",
-    description: "Proportion of neighborhood households receiving public assistance in the form of Supplemental Security Income (SSI), cash public assistance and/or Food stamps/SNAP."
+    description: "Proportion of neighborhood households receiving public assistance in the form of Supplemental Security Income (SSI), cash public assistance and/or Food stamps/SNAP.",
+    domain: "Employment"
   }, {
     key: '22',
     indicator: "Travel Time to Work",
-    description: "Average amount of time (in minutes) it takes a neighborhood resident to commute to work."
+    description: "Average amount of time (in minutes) it takes a neighborhood resident to commute to work.",
+    domain: "Employment"
   }, {
     key: '23',
     indicator: "Proximity to Brownfield Sites",
-    description: "Proportion of the neighborhood population located near a Brownfield site."
+    description: "Proportion of the neighborhood population located near a Brownfield site.",
+    domain: "Environmental"
   }, {
     key: '24',
     indicator: "Proximity to Superfund Sites",
-    description: "Proportion of neighborhood population located in close proximity to an active Superfund site."
+    description: "Proportion of neighborhood population located in close proximity to an active Superfund site.",
+    domain: "Environmental"
   }, {
     key: '25',
     indicator: "Residential Proximity to Traffic",
-    description: "Proportion of a neighborhood population located near roads with heavy traffic."
+    description: "Proportion of a neighborhood population located near roads with heavy traffic.",
+    domain: "Environmental"
   }, {
     key: '26',
     indicator: "School Proximity to Traffic",
-    description: "Proportion of neighborhood schools located near roads with a high volume of traffic."
+    description: "Proportion of neighborhood schools located near roads with a high volume of traffic.",
+    domain: "Environmental"
   }, {
     key: '27',
     indicator: "Toxic Releases from Facilities",
-    description: "Proportion of the neighborhood population located near industrial facilities reporting toxic air emissions."
+    description: "Proportion of the neighborhood population located near industrial facilities reporting toxic air emissions.",
+    domain: "Environmental"
   }, {
     key: '28',
     indicator: "Chronic School Absence",
-    description: "Proportion of students chronically absent from neighborhood schools (i.e., missing more than 10% school days)."
+    description: "Proportion of students chronically absent from neighborhood schools (i.e., missing more than 10% school days).",
+    domain: "Health & Public Safety"
   }, {
     key: '29',
     indicator: "Low Birth Weight",
-    description: "Proportion of infants born with low birth weight (< 2500 grams)."
+    description: "Proportion of infants born with low birth weight (< 2500 grams).",
+    domain: "Health & Public Safety"
   }, {
     key: '30',
     indicator: "Motor Vehicle Collisions",
-    description: "Annual incidence of motor vehicle collision injuries and fatalities per 1000 people for all modes of transportation on public roadways and right of ways."
+    description: "Annual incidence of motor vehicle collision injuries and fatalities per 1000 people for all modes of transportation on public roadways and right of ways.",
+    domain: "Health & Public Safety"
   }, {
     key: '31',
     indicator: "Pedestrian & Bicycle Injuries by Motor Vehicles",
-    description: "The annual number of injuries to a pedestrian or bicyclist by a motor vehicle on public roadways and rights of way per 1,000 people in a neighborhood."
+    description: "The annual number of injuries to a pedestrian or bicyclist by a motor vehicle on public roadways and rights of way per 1,000 people in a neighborhood.",
+    domain: "Health & Public Safety"
   }, {
     key: '32',
     indicator: "Preventable Hospitalizations",
-    description: "Age-adjusted hospitalization rate for conditions where appropriate outpatient care prevents or reduces the need for a patient to be admitted to the hospital."
+    description: "Age-adjusted hospitalization rate for conditions where appropriate outpatient care prevents or reduces the need for a patient to be admitted to the hospital.",
+    domain: "Health & Public Safety"
   }, {
     key: '33',
     indicator: "Public Health Nuisances",
-    description: "The number of reported public health nuisances per 1,000 residents."
+    description: "The number of reported public health nuisances per 1,000 residents.",
+    domain: "Health & Public Safety"
   }, {
     key: '34',
     indicator: "Violent Crime",
-    description: "Annual rate of reported incidents of violent crime per 1000 residents."
+    description: "Annual rate of reported incidents of violent crime per 1000 residents.",
+    domain: "Health & Public Safety"
   }, {
     key: '35',
     indicator: "Age of Housing",
-    description: "Proportion of neighborhood housing built prior to 1980."
+    description: "Proportion of neighborhood housing built prior to 1980.",
+    domain: "Housing"
   }, {
     key: '36',
     indicator: "Blood Lead Levels in Children",
-    description: "Proportion of children tested for lead with a blood level over 5 µg/dL."
+    description: "Proportion of children tested for lead with a blood level over 5 µg/dL.",
+    domain: "Housing"
   }, {
     key: '37',
     indicator: "Excessive Housing Cost Burden",
-    description: "Proportion of neighborhood households whose housing costs are 35% or more of their gross income."
+    description: "Proportion of neighborhood households whose housing costs are 35% or more of their gross income.",
+    domain: "Housing"
   }, {
     key: '38',
     indicator: "Vacancy Rates",
-    description: "Proportion of vacant residential properties."
+    description: "Proportion of vacant residential properties.",
+    domain: "Housing"
   }, {
     key: '39',
     indicator: "Access to Parks and Open Space",
-    description: "The proportion of a neighborhood's population within a half mile (10 minute walk) of a park."
+    description: "The proportion of a neighborhood's population within a half mile (10 minute walk) of a park.",
+    domain: "Natural Areas"
   }, {
     key: '40',
     indicator: "Tree Cover",
-    description: "Proportion of the neighborhood with tree coverage."
+    description: "Proportion of the neighborhood with tree coverage.",
+    domain: "Natural Areas"
   }, {
     key: '41',
     indicator: "Food Desert",
-    description: "Proportion of a neighborhood that is low income and has no access to affordable, healthy food."
+    description: "Proportion of a neighborhood that is low income and has no access to affordable, healthy food.",
+    domain: "Characteristics"
   }, {
     key: '42',
     indicator: "Land Use Mix",
-    description: "The average neighborhood-level diversity of destinations based on the mix of eight different employment types (office, retail, industrial, service, entertainment, education, health, and public sector)."
+    description: "The average neighborhood-level diversity of destinations based on the mix of eight different Employment types (office, retail, industrial, service, entertainment, education, health, and public sector).",
+    domain: "Characteristics"
   }, {
     key: '43',
     indicator: "Offsite Alcohol Outlets",
-    description: "Number of stores selling alcohol for “off-site” consumption per 1,000 people."
+    description: "Number of stores selling alcohol for “off-site” consumption per 1,000 people.",
+    domain: "Characteristics"
   }, {
     key: '44',
     indicator: "Walkability",
-    description: "A score of how pedestrian-friendly, i.e., walkable to amenities and common destinations, the neighborhood is."
+    description: "A score of how pedestrian-friendly, i.e., walkable to amenities and common destinations, the neighborhood is.",
+    domain: "Characteristics"
   }, {
     key: '45',
     indicator: "Residential Mobility",
-    description: "Proportion of neighborhood residents, age one year and older, living in the same house as the previous year."
+    description: "Proportion of neighborhood residents, age one year and older, living in the same house as the previous year.",
+    domain: "Social Cohesion"
   }, {
     key: '46',
     indicator: "Voter Participation",
-    description: "Proportion of voting eligible neighborhood residents who voted in the last election."
+    description: "Proportion of voting eligible neighborhood residents who voted in the last election.",
+    domain: "Social Cohesion"
   }, {
     key: '47',
     indicator: "Commute Mode Share",
-    description: "Proportion of neighborhood residents commuting to work by transit, bicycle, foot, or carpool."
+    description: "Proportion of neighborhood residents commuting to work by transit, bicycle, foot, or carpool.",
+    domain: "Transportation"
   }, {
     key: '48',
     indicator: "Household Transportation Costs",
-    description: "Proportion of household income spent on transportation."
+    description: "Proportion of household income spent on transportation.",
+    domain: "Transportation"
   }, {
     key: '49',
     indicator: "Pedestrian Connectivity",
-    description: "Density of pedestrian-oriented intersections within a neighborhood (i.e., a measure of how well sidewalks and streets are connected, and intersections spaced to allow residents to easily walk or bike around the neighborhood)."
+    description: "Density of pedestrian-oriented intersections within a neighborhood (i.e., a measure of how well sidewalks and streets are connected, and intersections spaced to allow residents to easily walk or bike around the neighborhood).",
+    domain: "Transportation"
   }, {
     key: '50',
     indicator: "Transit Accessibility",
-    description: "Frequency of transit service within a .25-mile during peak evening hours."
+    description: "Frequency of transit service within a .25-mile during peak evening hours.",
+    domain: "Transportation"
   }
 ];
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import IndexPage from './routes/IndexPage';
 import NeighborhoodPage from './routes/NeighborhoodPage';
 import IndicatorsPage from './routes/IndicatorsPage';
 import SingleNeighborhoodPage from './routes/SingleNeighborhoodPage';
+import SingleNeighborhoodChartPage from './routes/SingleNeighborhoodChartPage';
 import SingleIndicatorPage from './routes/SingleIndicatorPage';
 
 
@@ -14,6 +15,7 @@ function RouterConfig({ history }) {
         <Route path="/" exact component={IndexPage} />
         <Route path="/neighborhood" exact component={NeighborhoodPage} />
         <Route path="/neighborhood/:neighborhood" exact component={SingleNeighborhoodPage} />
+        <Route path="/neighborhood/:neighborhood/chart" exact component={SingleNeighborhoodChartPage} />
         <Route path="/indicators" exact component={IndicatorsPage} />
         <Route path="/indicators/:indicator" exact component={SingleIndicatorPage} />
       </Switch>
